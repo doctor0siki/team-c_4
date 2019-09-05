b3683c61fc81a09ecb6e2cfb1ad1199415fd4b2e
Takatoshi <taka.i1417s@gmail.com>
2019-09-05 18:03:11 +0900

    profile 変更テスト7

--- app/Controller/item/list.php
+++ app/Controller/item/list.php
@@ -5,14 +5,14 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // そのユーザーの投稿一覧を出すコントローラです
-$app->get('/item/list/', function (Request $request, Response $response) {
+$app->get('/item/list/{user_id}', function (Request $request, Response $response, $args) {
 
     $data=[];
 
     //アイテムDAOをインスタンス化します。
     $item = new Item($this->db);
 
-    $param["user_id"]="bbb";
+    $param["user_id"]=args["user_id"];
     //アイテム一覧を取得し、戻り値をresultに格納します
     $data["result"] = $item->getItemUser($param["user_id"]);
 
