ed0d31ffb5eb432996cad2d0654ea64fbff4a4e9
Takatoshi <taka.i1417s@gmail.com>
2019-09-05 17:01:47 +0900

    change

--- app/Controller/post/upload.php
+++ app/Controller/post/upload.php
@@ -15,15 +15,15 @@ $app->post('/upload', function (Request $request, Response $response) {
 print $sql;
 
   $filename = $_FILES['file_upload']['name'];
-$extension = array();
-$extension = explode('.', $filename);
+  $extension = array();
+  $extension = explode('.', $filename);
   $newfilename = date("YmdHis").".".$extension[1];
-    $upload = __DIR__.'/../../../img/'.$newfilename;
-    if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $upload)){
-  echo 'アップロード完了';
-}else{
-  echo 'アップロード失敗';
-}
+  $upload = __DIR__.'/../../../public/assets/img/'.$newfilename;
+  if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $upload)){
+    echo 'アップロード完了';
+  }else{
+    echo 'アップロード失敗';
+  }
 
 /*$data = $request->getParsedBody();
 $item = new Item($this->db);
