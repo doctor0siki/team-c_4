02af91d1fa17aeaae22f94516d5edda2fa83662e
guralin <tellurium38@gmail.com>
2019-09-05 14:40:33 +0900

    test用,dbにアクセスしたい

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -2,13 +2,17 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\Item;
 
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
+    $item = new Item($this->db);
 
-    $data = [];
+    $param["user_id"]="bbb";
 
     // Render index view
+    $reslut = $item->select($param,"","",1,false);
+    dd($result);
     return $this->view->render($response, 'top/index.twig', $data);
 });
 
@@ -20,4 +24,3 @@ $app->get('/sample', function (Request $request, Response $response) {
     // Render index view
     return $this->view->render($response, 'sample/index.twig', $data);
 });
-
